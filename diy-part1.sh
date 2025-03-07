4082c3a86f733fa64c71f941b5345090097ea304
realqhc <caiqihan021@hotmail.com>
2025-03-07 17:54:19 +1100

    Changing feeds.conf to use Github.com

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,6 +7,7 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=============================================================
+sed -e 's,git.openwrt.org/feed,github.com/openwrt,g; s,git.openwrt.org/project,github.com/openwrt,g' feeds.conf.default
 echo "src-git openwrt_passwall https://github.com/xiaorouji/openwrt-passwall.git" >> "feeds.conf.default"
 echo "src-git openwrt_passwall2 https://github.com/xiaorouji/openwrt-passwall2.git" >> "feeds.conf.default"
 echo "src-git openwrt_passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git" >> "feeds.conf.default"
