ff33b448858881a9b77953b098f4ac80401ccbfc
Qihan Cai <98016705+realqhc@users.noreply.github.com>
2026-09-02 00:02:22 +1000

    Pin Golang feed to 1.26

--- diy-part2.sh
+++ diy-part2.sh
@@ -61,7 +61,8 @@
 # git clone https://github.com/liuzhuoling2011/baidupcs-web.git package/lean/baidupcs-web
 
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+# Go 1.27 is currently incompatible with sing-box's go-json-experiment dependency.
+git clone --branch 26.x --single-branch https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 sed -i 's/$(TAR_OPTIONS)/$(TAR_OPTIONS) --no-same-owner/' include/unpack.mk
 # https://github.com/ZqinKing/wrt_release/commit/24fdb2727ababbea7b8c82b34c0b3def5ed6672c#diff-e14594f6bb9b29ac0146c04e78ae33e8e92f2f00709fefab97359dea31b94d11
 sed -i 's/download-ci-llvm=true/download-ci-llvm=false/g' "feeds/packages/lang/rust/Makefile"
